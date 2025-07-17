2b3a223b792f5f108b6928303a62c760775a3330
Clara Miño <claraa.mino12@gmail.com>
2025-07-17 08:01:56 -0300

    mejoras en la lógica de añadir al carrito. también agregué para que se vean los precios en el carrito

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*MEJORAS FUTURAS
     LocalStorage para el carrito
-    Ver precios en carrito y una suma total al final(reduce)
+    falta suma total al final(reduce)
     poder buscar un libro(filter)
 */
 
@@ -45,8 +45,6 @@ if(productos.length === 0){ //si no hay libros muestra no hay publicaciones disp
     subtitulo.innerText = "no hay publicaciones disponibles"
 }else{ //si hay libros, los crea dinamicamente en el dom
 
-
-    //ESTO LO INVESTIGUÉ YA QUE ME DABA ERROR PONER precioElemento.innerText = "$"+producto.precio 
     //formato de moneda:
     const opcionesFormato = {
         style: 'currency', // Indica que hay formato de moneda
@@ -60,59 +58,54 @@ if(productos.length === 0){ //si no hay libros muestra no hay publicaciones disp
 
     productos.forEach(producto=>{//crea tantos productos como haya en el array
 
-    const nuevoProducto = document.createElement("div"); //crea un div
-    nuevoProducto.classList.add("producto"); //le asigno la clase producto
-    
-    const link = document.createElement("a"); //crea un link
-    link.setAttribute("href","pages/detalle-producto.html") //le asigno un enlace
-    link.setAttribute("target","_blank") //El enlace se va a abrir en otra pestaña
+        const nuevoProducto = document.createElement("div"); //crea un div
+        nuevoProducto.classList.add("producto"); //le asigno la clase producto
+        
+        const link = document.createElement("a"); //crea un link
+        link.setAttribute("href","pages/detalle-producto.html") //le asigno un enlace
+        link.setAttribute("target","_blank") //El enlace se va a abrir en otra pestaña
 
-    const tituloProducto = document.createElement("h3"); //crea el titulo del producto
-    tituloProducto.classList.add("producto-titulo"); //le asigno la clase producto-titulo
-    tituloProducto.innerText = producto.titulo //valor del titulo
-    
-    const img = document.createElement("img"); //crea una imagen
-    img.setAttribute("src",producto.imagen) //le asigno la imagen
-    img.classList.add("producto-img");
-
-
-    const precioElemento = document.createElement("p"); //crea un p para mostrar los precios
-    precioElemento.innerText = formateador.format(producto.precio);//asigna los valores a p
-    precioElemento.classList.add("producto-precio");
+        const tituloProducto = document.createElement("h3"); //crea el titulo del producto
+        tituloProducto.classList.add("producto-titulo"); //le asigno la clase producto-titulo
+        tituloProducto.innerText = producto.titulo //valor del titulo
+        
+        const img = document.createElement("img"); //crea una imagen
+        img.setAttribute("src",producto.imagen) //le asigno la imagen
+        img.classList.add("producto-img");
 
 
-    const button = document.createElement("button"); //crea un boton
-    button.classList.add("producto-boton");
-    button.innerText = "Añadir al carrito"; //valor del boton
+        const precioElemento = document.createElement("p"); //crea un p para mostrar los precios
+        precioElemento.innerText = formateador.format(producto.precio);//asigna los valores a p
+        precioElemento.classList.add("producto-precio");
 
 
-    //inserta la estructura html de libros al dom
-    seccionProductos.append(nuevoProducto); 
-    link.append(tituloProducto,img);
-    nuevoProducto.append(link,precioElemento,button)
+        const button = document.createElement("button"); //crea un boton
+        button.classList.add("producto-boton");
+        button.innerText = "Añadir al carrito"; //valor del boton
+        button.addEventListener("click",()=>{ //le agrego un evento a cada botón para poder añadir al carrito
+            anadirAlCarrito(producto) //llama a la lógica para añadir al carrito
+        })
+
+
+        //inserta la estructura html de libros al dom
+        seccionProductos.append(nuevoProducto); 
+        link.append(tituloProducto,img);
+        nuevoProducto.append(link,precioElemento,button)
+        
     })
 }
     //      ----LOGICA CARRITO------
-    const carrito = []; //carrito inicial vacio
+    
+    const carrito = []; //carrito inicial vacio (array de objetos)
     const listaCarrito = document.getElementById("lista-carrito");//ul
     const boton = document.getElementsByClassName("producto-boton")// Selecciona todos los elementos con la clase 'producto-boton'
     const itemVacio = document.createElement("li");//crea un item para mostrar el carrito vacio
 
-    //le crea un evento a cada boton
-    for (let i = 0; i < boton.length; i++) {
-        boton[i].addEventListener('click', function() {
+    function anadirAlCarrito(producto){ //recibe(objeto) el producto clickeado
+        carrito.push(producto)
+        actualizarCarritoDOM()
+    }
 
-            /*acá lo que estoy haciendo es moverme en la jerarquia para poder llegar al h3 que contiene el titulo del libro para luego usarlo en el push*/
-            const pProducto = this.previousElementSibling;
-            const aProducto = pProducto.previousElementSibling;
-            const nombreProducto = aProducto.firstElementChild;
-           
-            carrito.push(nombreProducto.innerText) //aca se deberia mostrar el nombre del producto con su precio y luego un total
-            actualizarCarritoDOM();
-    })};
-    
-    
-    
     function actualizarCarritoDOM(){
         if(carrito.length === 0){ //si el carrito está vacío dice que no hay productos
             itemVacio.innerText = "Aún no hay productos en el carrito"
@@ -121,23 +114,13 @@ if(productos.length === 0){ //si no hay libros muestra no hay publicaciones disp
             
             listaCarrito.innerHTML = ''; //limpia el carrito
             
-            // 2. Iterar sobre cada producto del carrito
+            //Iterar sobre cada producto del carrito
             carrito.forEach(producto => {
                 const itemCarrito = document.createElement("li");//por cada producto del array crea un item de carrito
-                itemCarrito.innerText = producto; // Asigna el nombre del producto al li
+                itemCarrito.innerText = producto.titulo + " $" +producto.precio; // Asigna el nombre del producto y precio al li
                 listaCarrito.append(itemCarrito); // muestra el li en el dom
             });
         }
     }
     
-   
-
 actualizarCarritoDOM(); //muestra que el carrito está vacío al cargar la página
-
-
-
-
-
-
-
-
